ef621a919647419c425371d694b13dfbfd170b2a
unknown <almusallam296@hotmail.com>
2023-05-18 07:32:56 -0700

    Done

--- index.js
+++ index.js
@@ -1,4 +1,44 @@
-const employeesJSON = require('./employees.json');
-const managersJSON = require('./managers.json');
+const employeesJSON = require("./employees.json");
+const managersJSON = require("./managers.json");
 
-// Code Here
+class Employee {
+  constructor(name, position, yearJoined, salary) {
+    this.name = name;
+    this.position = position;
+    thsi.yearJoined = yearJoined;
+    this.salary = salary;
+  }
+  idBadge() {
+    return this.position + " " + this.name;
+  }
+}
+
+class Maneger extends Employee {
+  constructor(name, position, yearJoined, salary, bonusPercentage) {
+    super(name, position, yearJoined, salary);
+    this.bonusPercentage = bonusPercentage;
+  }
+  salaryIncrese(increaseAmount) {
+    return increaseAmount + this.salary;
+  }
+  logMangerInfo() {
+    console.log(this.name);
+    console.log("since" + this.yearJoined);
+    console.log("Bonus % " + this.bonusPercentage);
+  }
+  getWorkingYears(Year) {
+    return this.yearJoined - Year;
+  }
+}
+
+employeesJSON = new Employee.map([
+  ("Ahmad", "Eng", "2024", 1600),
+  ("Osama", "HR", "2025", "2000"),
+  ("Wahab", "IT", "2026", "2200"),
+]);
+managersJSON = new Maneger.map([
+  ("Ali", "Eng", "2024", 1600, 20),
+  ("Aziz", "HR", "2025", "2000", 10),
+  ("Adel", "IT", "2026", "2200", 30),
+]);
+console.log(idBadge(employeesJSON.forEach()));
